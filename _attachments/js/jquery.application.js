e0f0a1b2f47b0f83a944586dd34fd9dd734bfe49
Jan Pieper <kontakt@jan-pieper.info>
2011-01-25 22:34:41 +0100

    removed some lines that aren't needed anymore

--- _attachments/js/jquery.application.js
+++ _attachments/js/jquery.application.js
@@ -1,52 +1,6 @@
 (function($) {
     $.application = $.application || {};
     $.extend($.application, {
-        run: function() {
-            $.couch.db("efatal").view("efatal/errors-by-date", {
-                limit: 7,
-                group_level: 2,
-                descending: true,
-                success: function(data) {
-                    var list = $("#dates");
-                    if (data.rows.length) {
-                        for (var index in data.rows) {
-                            list.append($.application.item(
-                                data.rows[index]
-                            ));
-                        }
-                        $("li a", list).first().click();
-                    }
-                }
-            });
-        },
-        item: function(row) {
-            return $("<li>").append(
-                $("<a>").attr({
-                    "title": row.value + " error" + ((row.value != 1) ? "s" : ""),
-                    "href": "#" + row.key
-                }).text(
-                    $.date.format(row.key, "d. mmm yyyy")
-                ).click(function() {
-                    var dateKey = $(this).attr("href").substr(1);
-                    $("#dates li.active").first().removeClass("active");
-                    $.couch.db("efatal").view("efatal/recent-errors", {
-                        key: dateKey,
-                        limit: 100,
-                        descending: true,
-                        success: function(data) {
-                            var list = $("#errors").empty();
-                            for (var index in data.rows) {
-                                list.append($.application.entry(
-                                    data.rows[index].value
-                                ));
-                            }
-                            $("#total").text(data.rows.length);
-                        }
-                    });
-                    $(this).parent().addClass("active");
-                })
-            );
-        },
         entry: function(doc) {
             return $("<table>").attr({
                 "id": "doc_" + doc._id,
